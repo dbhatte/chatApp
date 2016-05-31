a357219ee64aca810a313fce6f5fbf27d1486750
Devendra Bhatte <devendra.bhatte@home24.de>
2016-05-31 15:35:21 +0200

    added mongodb support

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+var mongoose = require('mongoose');
+var messages = require('./routes/index');
+
+// default to a 'localhost' configuration:
+var connection_string = '127.0.0.1:27017/chatApp';
+// if OPENSHIFT env variables are present, use the available connection info:
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
+  connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+      process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+      process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+      process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+      process.env.OPENSHIFT_APP_NAME;
+}
+
+mongoose.connect('mongodb://' + connection_string);
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use('/messages',  messages);
+
+//app.use('/', router);
+app.use(express.static(__dirname + '/public'));
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
+
 
 var userArray = [];
 /*
@@ -21,8 +45,6 @@ server.listen(port, ip_address, function () {
 });
 
 
-app.use(express.static(__dirname + '/public'));
-app.use('/bower_components', express.static(__dirname + '/bower_components'));
 
 
 io.on('connection', function(socket){
